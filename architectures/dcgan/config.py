79b4e04db07bb6d6d3930a87aabe35342c84a9aa
Jan Karaś <mygnirut@gmail.com>
2023-08-16 14:23:16 +0200

    Add things to config file.

--- architectures/dcgan/config.py
+++ architectures/dcgan/config.py
@@ -1,9 +1,14 @@
 import torch
 
-EPOCHS = 1
+# Training
+EPOCHS = 100
 BATCH_SIZE = 32  # In paper a batch of 128 was used
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Model
 LEAKY_RELU_SLOPE = 0.2
+HIDDEN_UNITS = 10
+Z_DIM = 100
 
 
 # Optimizer (for details see papers section IV "DETAILS OF ADVERSARIAL TRAINING")
